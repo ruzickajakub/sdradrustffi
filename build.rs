b91ad93e7705700053cea85128f85850eeae1630
Jakub Ruzicka <jakub.ruzicka@ericsson.com>
2025-02-21 18:44:43 +0100

    Simplify the script

--- build.rs
+++ build.rs
@@ -1,20 +1,16 @@
 use std::process::Command;
 use std::path::Path;
-use std::env;
 
 fn main() {
-    let sdrad_srcdir = Path::new("./secure-rewind-and-discard/src");
-    let output_prefix = env::var("OUT_DIR").unwrap();
-    let libsdrad_path = Path::new(&output_prefix);
-    let original_ld_library_path = env::var("LD_LIBRARY_PATH").unwrap_or_default();
+    let sdrad_cdir = Path::new("./secure-rewind-and-discard/src");
     
     Command::new("sh")
             .arg("-c")
             .arg("make")
-            .env("OUTPUT_PREFIX", &output_prefix)
-            .current_dir(&sdrad_srcdir)
+            .current_dir(&sdrad_dir)
             .status().unwrap();
 
-    println!(r"cargo:rustc-link-search={}", &libsdrad_path.display());
-    println!(r"cargo:rustc-env=LD_LIBRARY_PATH={}:{}", &libsdrad_path.display(), &original_ld_library_path);
+    let original_ld_library_path = env::var("LD_LIBRARY_PATH").unwrap_or_default();
+    println!(r"cargo:rustc-link-search={}", &sdrad_dir.display());
+    println!(r"cargo:rustc-env=LD_LIBRARY_PATH={}:{}", &sdrad_dir.display(), &original_ld_library_path);
 }
